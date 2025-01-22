6c2cc3396ed4daebd4b6f630c48b7c5600bc92cf
zhangwei <zhangluther@yahoo.com>
2025-01-22 10:08:31 +0800

    服务器可用

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
+
 const app = express();
 const port = 3000;
+
+// 静态资源文件夹
+app.use(express.static('public'))
 // 处理 GET 请求
 app.get('/', (req, res) => {
   res.send('Hello World！');
 });
+
 // 启动服务器
 app.listen(port, () => {
   console.log(`服务器运行在 // 启动服务器`);
